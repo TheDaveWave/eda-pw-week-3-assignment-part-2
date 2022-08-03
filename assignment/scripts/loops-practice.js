ea99dbc5b84fdf7ed1f9723de1bf8a80ca4c6c6e
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-03 07:54:38 -0500

    completed problem 1a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,6 +13,10 @@ for (let i = 0; i < 4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+// I would only need to change the condition.
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
